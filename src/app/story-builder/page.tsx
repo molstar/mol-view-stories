afc0418c122dbcafc3130c07b8f4951e26bda63f
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2025-05-28 17:13:21 -0400

    manubar

--- src/app/story-builder/page.tsx
+++ src/app/story-builder/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import React, { useState } from "react";
 import { Header } from "@/components/layout/Header";
 import { Footer } from "@/components/layout/Footer";
 import { SceneEditors } from "@/components/story-builder/SceneEditors";
