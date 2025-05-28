c6f246560c944ab7365adebd589f7c491b892517
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2025-05-28 10:01:19 -0400

    remove the Description box

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,26 +15,7 @@ import { SceneList } from "./components/SceneList";
 
 
 
-function DescriptionBox() {
-  const [scenes] = useAtom(ScenesAtom);
-  const [activeSceneId] = useAtom(ActiveSceneIdAtom);
-  
-  const activeScene = getActiveScene(scenes, activeSceneId);
 
-  return (
-    <div className="description-box">
-      <h3 className="description-title">Scene Description</h3>
-      {activeScene ? (
-        <div>
-          <h4 className="description-scene-title">{activeScene.header}</h4>
-          <p className="description-scene-text">{activeScene.description}</p>
-        </div>
-      ) : (
-        <p className="description-placeholder">Select a scene to view its description</p>
-      )}
-    </div>
-  );
-}
 
 export default function Home() {
   return (
@@ -50,7 +31,6 @@ export default function Home() {
         {/* Left Column */}
         <div className="left-column">
           <SceneList />
-          <DescriptionBox />
           <div className="editors-container">
             <div className="editor-section">
               <MonacoMarkdownEditor />
