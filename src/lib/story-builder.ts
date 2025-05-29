8bec16a46a76d0b5ba59057b06938f1b85843528
dsehnal <david.sehnal@gmail.com>
2025-05-29 17:17:24 +0200

    adjust camera positions to prevent shifts

--- src/lib/story-builder.ts
+++ src/lib/story-builder.ts
@@ -1,5 +1,7 @@
 import { MVSData } from 'molstar/lib/extensions/mvs/mvs-data';
 import { SceneData, StoryMetadata } from '../app/state/types';
+import { Camera } from 'molstar/lib/mol-canvas3d/camera';
+import { Vec3 } from 'molstar/lib/mol-math/linear-algebra';
 
 const createStateProvider = (code: string) => {
   return new Function('builder', code);
@@ -12,7 +14,7 @@ export function getMVSSnapshot(scene: SceneData) {
     stateProvider(builder);
     if (scene.camera) {
       builder.camera({
-        position: scene.camera.position as unknown as [number, number, number],
+        position: adjustedCameraPosition(scene.camera),
         target: scene.camera.target as unknown as [number, number, number],
         up: scene.camera.up as unknown as [number, number, number],
       });
@@ -30,6 +32,13 @@ export function getMVSSnapshot(scene: SceneData) {
     console.error('Error creating state provider:', error);
     throw error;
   }
+}
+
+function adjustedCameraPosition(camera: Camera.Snapshot) {
+  // MVS uses FOV-adjusted camera position, need to apply inverse here so it doesn't offset the view when loaded
+  const f = camera.mode === 'orthographic' ? 1 / (2 * Math.tan(camera.fov / 2)) : 1 / (2 * Math.sin(camera.fov / 2));
+  const delta = Vec3.sub(Vec3(), camera.position, camera.target);
+  return Vec3.scaleAndAdd(Vec3(), camera.target, delta, 1 / f) as unknown as [number, number, number];
 }
 
 export async function getMVSData(story: StoryMetadata, scenes: SceneData[]): Promise<MVSData | Uint8Array> {
