9e162bc2803d4f403fbdfb3fe355af77b35fd7c7
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2025-05-31 10:19:05 -0400

    update build config to handle alias on molstar

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
+  basePath: process.env.NODE_ENV === "production" ? "/mol-view-stories" : "",
   images: {
     unoptimized: true,
   },
