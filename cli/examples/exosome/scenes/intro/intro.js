62e20efa53e23a6a6706d406385b3e309ae9d545
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2025-10-26 09:43:14 -0400

    exosome tidy updates

--- cli/examples/exosome/scenes/intro/intro.js
+++ cli/examples/exosome/scenes/intro/intro.js
@@ -2,71 +2,65 @@
 Object.entries(surfaceChainIds).forEach(([chainId, proteinName]) => {
   structure
     .component({
-      selector: { label_asym_id: chainId }
+      selector: { label_asym_id: chainId },
     })
-    .representation({ ref: proteinName, type: default_representation, custom: granularity
-    })
-    .color({ custom: { molstar_color_theme_name: "chain-id" } });
+    .representation({ ref: proteinName, type: default_representation, custom: granularity })
+    .color({ custom: { molstar_color_theme_name: 'chain-id' } });
 });
 
 // Loop through each chain and create spacefill representation with named ref
 Object.entries(interiorChainIds).forEach(([chainId, proteinName]) => {
   structure
     .component({
-      selector: { label_asym_id: chainId }
+      selector: { label_asym_id: chainId },
     })
     .representation({ ref: proteinName, type: default_representation, custom: granularity })
-    .color({ custom: { molstar_color_theme_name: "chain-id" } });
+    .color({ custom: { molstar_color_theme_name: 'chain-id' } });
 });
-
 
 Object.entries(fiberChainIds).forEach(([chainId, proteinName]) => {
   structure
     .component({
-      selector: { label_asym_id: chainId }
+      selector: { label_asym_id: chainId },
     })
-    .representation({ ref: proteinName, type: default_representation, custom: granularity})
-    .color({ custom: { molstar_color_theme_name: "chain-id" } });
+    .representation({ ref: proteinName, type: default_representation, custom: granularity })
+    .color({ custom: { molstar_color_theme_name: 'chain-id' } });
 });
 
 //same for lipids
 structure
   .component({
-    selector: { label_comp_id: 'LIP' }
+    selector: { label_comp_id: 'LIP' },
   })
-  .representation({ref:'lipids', type: 'spacefill', custom: granularity})
-  .color({ color: 'grey' });  
-
+  .representation({ ref: 'lipids', type: 'spacefill', custom: granularity })
+  .color({ color: 'grey' });
 
 const primitives = builder.primitives({
-    label_attachment: 'bottom-left',
-    label_show_tether: false,
-    label_tether_length: 2,
-    label_background_color: 'black',
-    // snapshot_key: 'A',
-    custom: {
-        molstar_markdown_commands: {
-            'apply-snapshot': 'AB',
-            'play-audio': 'Exosome_intro.mp3',
-        }
-    }
+  label_attachment: 'bottom-left',
+  label_show_tether: false,
+  label_tether_length: 2,
+  label_background_color: 'black',
+  // snapshot_key: 'A',
+  custom: {
+    molstar_markdown_commands: {
+      'apply-snapshot': 'AB',
+      'play-audio': 'Exosome_intro.mp3',
+    },
+  },
 });
 
-
 primitives.label({
-    position: [0,700,0],
-    text: 'Start Tour',
-    label_size: 150.0,
-    label_offset: 2.0   // Offset to avoid overlap with the residue  
-  })
+  position: [0, 700, 0],
+  text: 'Start Tour',
+  label_size: 150.0,
+  label_offset: 2.0, // Offset to avoid overlap with the residue
+});
 
-
-const anim = builder.animation(
-    {
-        custom: {
-        molstar_trackball: {
-            name: 'spin',
-            params: { speed: 0.05 },
-        }
-    }}
-);
+const anim = builder.animation({
+  custom: {
+    molstar_trackball: {
+      name: 'spin',
+      params: { speed: 0.05 },
+    },
+  },
+});
